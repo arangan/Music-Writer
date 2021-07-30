9bf1701a89927df744bf2bf202210c9ece553765
Arvind <3113987+arangan@users.noreply.github.com>
2021-07-30 12:39:36 -0600

    added prettierrc
    changes to to eslint
    
    added sass

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,16 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    'no-multi-spaces': ['error'],
+    'vue/no-multiple-template-root': 'off',
+    'no-unused-vars': 1,
+    'no-use-before-define': 2,    
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+      }
+    ],    
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
