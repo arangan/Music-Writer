1f02fefecb2eafdc9d4f0293e47d676b72eb697e
arangan <3113987+arangan@users.noreply.github.com>
2021-07-29 21:58:04 -0600

    First Code Checkin

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import fs from "fs";
+//
+export default defineComponent({
+  name: "App",
+  components: {},
+  mounted() {
+    console.log(process.cwd());
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
